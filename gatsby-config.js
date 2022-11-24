c141971684cde2e807e16ad7fbcdc832026fc0f2
bibbyc2022 <118505436+bibbyc2022@users.noreply.github.com>
2022-11-24 21:24:27 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,17 +2,17 @@ module.exports = {
   pathPrefix: '/running_page', // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: 'Bibbyc Running Page',
-    siteUrl: 'https://yihong.run',
-    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+    siteUrl: 'https://twitter.com/ChenBibby',
+    logo: 'https://s1.ax1x.com/2022/11/24/zGGiee.jpg',
     description: 'Personal site and blog',
     navLinks: [
       {
-        name: 'Blog',
-        url: 'https://yihong.run/running',
+        name: '哔哩哔哩',
+        url: 'https://b23.tv/J8PLpuj',
       },
       {
-        name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        name: 'Twitter',
+        url: 'https://twitter.com/ChenBibby',
       },
     ],
   },
